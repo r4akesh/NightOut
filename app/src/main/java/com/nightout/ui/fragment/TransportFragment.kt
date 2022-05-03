d2e185e3b6c3169ad29dfdb1bdea78307b6fc010
rakesh <rakeshkushwaha@emails.emizentech.com>
2022-05-03 19:03:31 +0530

    open cab app

--- app/src/main/java/com/nightout/ui/fragment/TransportFragment.kt
+++ app/src/main/java/com/nightout/ui/fragment/TransportFragment.kt
@@ -1,8 +1,8 @@
 package com.nightout.ui.fragment
 
-import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.gms.actions.ReserveIntents
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -22,9 +21,10 @@ import com.nightout.adapter.SomeTaxiListAdapter
 import com.nightout.databinding.FragmentTransportLayoutBinding
 import com.nightout.interfaces.OnMenuOpenListener
 import com.nightout.ui.activity.TaxiListActivity
+import java.util.*
 
 
-class TransportFragment() : Fragment(), OnMapReadyCallback {
+class TransportFragment() : Fragment(), View.OnClickListener, OnMapReadyCallback {
     lateinit var binding: FragmentTransportLayoutBinding
     private var onMenuOpenListener:OnMenuOpenListener? = null
 
@@ -41,39 +41,156 @@ class TransportFragment() : Fragment(), OnMapReadyCallback {
         binding.arrowBtn.setOnClickListener {
             startActivity(Intent(requireContext(),TaxiListActivity::class.java))
         }
-        binding.trnsportGoBtn.setOnClickListener {
-            val pm: PackageManager = requireContext().getPackageManager()
-            /*  try {
-                  pm.getPackageInfo("com.ubercab", PackageManager.GET_ACTIVITIES)
-                  val uri = "uber://?action=setPickup&pickup=my_location"
-                  val intent = Intent(Intent.ACTION_VIEW)
-                  intent.data = Uri.parse(uri)
-                  startActivity(intent)
-              } catch (e: PackageManager.NameNotFoundException) {
-                  try {
-                      startActivity(
-                          Intent(
-                              Intent.ACTION_VIEW,
-                              Uri.parse("market://details?id=com.ubercab")
-                          )
-                      )
-                  } catch (anfe: ActivityNotFoundException) {
-                      startActivity(
-                          Intent(
-                              Intent.ACTION_VIEW,
-                              Uri.parse("http://play.google.com/store/apps/details?id=com.ubercab")
-                          )
-                      )
-                  }
-              }*/
-            val intent = Intent(ReserveIntents.ACTION_RESERVE_TAXI_RESERVATION)
-            if (intent.resolveActivity(pm) != null) {
-                startActivity(intent)
-            }
+        binding.transportUberConstrent.setOnClickListener {
+
         }
 
         init()
         return binding.root
+    }
+
+    override fun onClick(v: View?) {
+            if(v== binding.transportUberConstrent){
+                openUber()
+            }
+        else if(v==binding.transportOlaConstrent){
+            openOla()
+            }
+
+            else if(v==binding.transportLyftConstrent){
+            openLyft()
+            }
+
+            else if(v==binding.transportFreeNowConstrent){
+                openFreeNow()
+            }
+            else if(v==binding.transportGettConstrent){
+                openGett()
+            }
+
+    }
+
+    private fun openGett() {
+        val launchIntent: Intent? = requireContext().getPackageManager().getLaunchIntentForPackage("com.gettaxi.android")
+        if (launchIntent != null) {
+            startActivity(launchIntent) //null pointer check in case package name was not found
+        } else {
+            val uri = Uri.parse("market://details?id=com.gettaxi.android")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e:  Exception) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=com.gettaxi.android")
+                    )
+                )
+            }
+        }
+    }
+
+    private fun openFreeNow() {
+        val launchIntent: Intent? = requireContext().getPackageManager().getLaunchIntentForPackage("taxi.android.client")
+        if (launchIntent != null) {
+            startActivity(launchIntent) //null pointer check in case package name was not found
+        } else {
+            val uri = Uri.parse("market://details?id=taxi.android.client")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e:  Exception) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=taxi.android.client")
+                    )
+                )
+            }
+        }
+    }
+
+    private fun openLyft() {
+        val launchIntent: Intent? = requireContext().getPackageManager().getLaunchIntentForPackage("me.lyft.android")
+        if (launchIntent != null) {
+            startActivity(launchIntent) //null pointer check in case package name was not found
+        } else {
+            val uri = Uri.parse("market://details?id=me.lyft.android")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e:  Exception) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=me.lyft.android")
+                    )
+                )
+            }
+        }
+    }
+
+    private fun openOla() {
+        val launchIntent: Intent? = requireContext().getPackageManager().getLaunchIntentForPackage("com.olacabs.customer")
+        if (launchIntent != null) {
+            startActivity(launchIntent) //null pointer check in case package name was not found
+        } else {
+            val uri = Uri.parse("market://details?id=com.olacabs.customer")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e:  Exception) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=com.olacabs.customer")
+                    )
+                )
+            }
+        }
+    }
+
+    private fun openUber() {
+        val pm: PackageManager = requireContext().getPackageManager()
+        try {
+            pm.getPackageInfo("com.ubercab", PackageManager.GET_ACTIVITIES)
+           // val uri = "uber://?action=setPickup&pickup=my_location" // show locaton page
+            val uri = "uber://"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(uri)
+            startActivity(intent)
+        } catch (e: PackageManager.NameNotFoundException) {
+            try {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.ubercab")))
+            } catch (anfe:  Exception) {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=com.ubercab")))
+            }
+        }
+//            val intent = Intent(ReserveIntents.ACTION_RESERVE_TAXI_RESERVATION)
+//            if (intent.resolveActivity(pm) != null) {
+//                startActivity(intent)
+//            }
+
     }
 
     private fun init(){
@@ -81,6 +198,11 @@ class TransportFragment() : Fragment(), OnMapReadyCallback {
         mapFragment!!.getMapAsync(this)
         binding.headerSideMenu.setOnClickListener { onMenuOpenListener?.onOpenMenu() }
         setUpTaxiList()
+        binding.transportUberConstrent.setOnClickListener(this)
+        binding.transportOlaConstrent.setOnClickListener(this)
+        binding.transportLyftConstrent.setOnClickListener(this)
+        binding.transportGettConstrent.setOnClickListener(this)
+        binding.transportFreeNowConstrent.setOnClickListener(this)
 
     }
 
@@ -98,6 +220,8 @@ class TransportFragment() : Fragment(), OnMapReadyCallback {
         val someTaxiListAdapter = SomeTaxiListAdapter(requireContext())
         binding.nearTaxiList.adapter = someTaxiListAdapter
     }
+
+
 
 
 }
